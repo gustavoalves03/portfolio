4da20b72f6c303ef9bd6dfac11fd3c8eff9b7341
Gustavo <gustavo.alves@empirys.com>
2026-05-08 11:33:53 +0200

    feat(tenant): allow null name on entity

--- backend/src/main/java/com/prettyface/app/tenant/domain/Tenant.java
+++ backend/src/main/java/com/prettyface/app/tenant/domain/Tenant.java
@@ -24,7 +24,7 @@ public class Tenant {
     @Column(name = "slug", nullable = false)
     private String slug;
 
-    @Column(name = "name", nullable = false)
+    @Column(name = "name")
     private String name;
 
     @Column(name = "owner_id", nullable = false)
